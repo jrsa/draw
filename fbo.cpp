9af700b1b736723663e1b530efd600a088d041e0
James Anderson <jrsa@jrsa.co>
2016-09-01 01:05:42 -0700

    wrap fbo textures :)

--- fbo.cpp
+++ fbo.cpp
@@ -15,6 +15,8 @@ fbo::fbo(int h, int w): _colorbuffer_texture_obj(0), _fbo_obj(0) {
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (int)GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, (int)GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, (int)GL_REPEAT);
 
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _colorbuffer_texture_obj, 0);
 }
